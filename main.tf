5a71e7745552c31e28228eb7e040f3e8b676d17e
Dallas Slaughter <dslaughtr@gmail.com>
2019-10-01 11:39:41 -0700

    [bugfix] - Add provider and terraform blocks as Terragrunt 0.19 requires

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+# Previously, we didn't need this. Now, we do. How exciting.
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+
+  # The latest version of Terragrunt (v0.19.0 and above) requires Terraform 0.12.0 or above.
+  required_version = ">= 0.12.0"
+}
+
 module "label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.7.0"
   namespace  = var.namespace
