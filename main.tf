b0550ce93f8bac149113556a2e12335c9eb93577
Dallas <dslaughtr@gmail.com>
2019-11-20 16:11:01 -0800

    Change naming in main.tf
    For my purposes, the label module is not useful. Should remove. In the meantime, to get my stack working, I have done the dirty here and quickly edited it.

--- main.tf
+++ main.tf
@@ -12,8 +12,7 @@ terraform {
 }
 
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.15.0"
-#   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.7.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.7.0"
   namespace  = var.namespace
   name       = var.name
   stage      = var.stage
@@ -24,7 +23,7 @@ module "label" {
 }
 
 resource "aws_security_group" "default" {
-  count       = var.enabled ? 1 : 0
+  count       = var.enabled == "true" ? 1 : 0
   name        = module.label.id
   description = "Security Group for DocumentDB cluster"
   vpc_id      = var.vpc_id
@@ -32,7 +31,7 @@ resource "aws_security_group" "default" {
 }
 
 resource "aws_security_group_rule" "egress" {
-  count             = var.enabled ? 1 : 0
+  count             = var.enabled == "true" ? 1 : 0
   type              = "egress"
   description       = "Allow all egress traffic"
   from_port         = 0
@@ -42,20 +41,24 @@ resource "aws_security_group_rule" "egress" {
   security_group_id = join("", aws_security_group.default.*.id)
 }
 
+locals {
+  allowed_security_groups = [split(",", join(",", var.allowed_security_groups))]
+}
+
 resource "aws_security_group_rule" "ingress_security_groups" {
-  count                    = length(var.allowed_security_groups)
+  count                    = var.allowed_security_groups_length
   type                     = "ingress"
   description              = "Allow inbound traffic from existing Security Groups"
   from_port                = var.db_port
   to_port                  = var.db_port
   protocol                 = "tcp"
-  source_security_group_id = var.allowed_security_groups[count.index]
+  source_security_group_id = element(local.allowed_security_groups, count.index)
   security_group_id        = join("", aws_security_group.default.*.id)
 }
 
 resource "aws_security_group_rule" "ingress_cidr_blocks" {
   type              = "ingress"
-  count             = var.enabled && length(var.allowed_cidr_blocks) > 0 ? 1 : 0
+  count             = var.enabled == "true" && length(var.allowed_cidr_blocks) > 0 ? 1 : 0
   description       = "Allow inbound traffic from CIDR blocks"
   from_port         = var.db_port
   to_port           = var.db_port
@@ -65,8 +68,8 @@ resource "aws_security_group_rule" "ingress_cidr_blocks" {
 }
 
 resource "aws_docdb_cluster" "default" {
-  count                           = var.enabled ? 1 : 0
-  cluster_identifier              = module.label.id
+  count                           = var.enabled == "true" ? 1 : 0
+  cluster_identifier              = var.name
   master_username                 = var.master_username
   master_password                 = var.master_password
   backup_retention_period         = var.retention_period
@@ -87,8 +90,8 @@ resource "aws_docdb_cluster" "default" {
 }
 
 resource "aws_docdb_cluster_instance" "default" {
-  count              = var.enabled ? var.cluster_size : 0
-  identifier         = "${module.label.id}-${count.index + 1}"
+  count              = var.enabled == "true" ? var.cluster_size : 0
+  identifier         = "${var.name}${count.index > 0 ? "-${count.index}" : ""}"
   cluster_identifier = join("", aws_docdb_cluster.default.*.id)
   apply_immediately  = var.apply_immediately
   instance_class     = var.instance_class
@@ -97,7 +100,7 @@ resource "aws_docdb_cluster_instance" "default" {
 }
 
 resource "aws_docdb_subnet_group" "default" {
-  count       = var.enabled ? 1 : 0
+  count       = var.enabled == "true" ? 1 : 0
   name        = module.label.id
   description = "Allowed subnets for DB cluster instances"
   subnet_ids  = var.subnet_ids
@@ -106,19 +109,23 @@ resource "aws_docdb_subnet_group" "default" {
 
 # https://docs.aws.amazon.com/documentdb/latest/developerguide/db-cluster-parameter-group-create.html
 resource "aws_docdb_cluster_parameter_group" "default" {
-  count       = var.enabled ? 1 : 0
+  count       = var.enabled == "true" ? 1 : 0
   name        = module.label.id
   description = "DB cluster parameter group"
   family      = var.cluster_family
-
   dynamic "parameter" {
-    for_each = var.cluster_parameters
+    for_each = [var.cluster_parameters]
     content {
+      # TF-UPGRADE-TODO: The automatic upgrade tool can't predict
+      # which keys might be set in maps assigned here, so it has
+      # produced a comprehensive set here. Consider simplifying
+      # this after confirming which keys can be set in practice.
+
+      apply_method = lookup(parameter.value, "apply_method", null)
       name         = parameter.value.name
       value        = parameter.value.value
     }
   }
-
   tags = module.label.tags
 }
 
@@ -130,24 +137,22 @@ locals {
 }
 
 module "dns_master" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.3.0"
-#   source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.2.6"
-  enabled   = var.enabled && length(var.zone_id) > 0 ? "true" : "false"
-#   namespace = var.namespace
+  source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.2.6"
+  enabled   = var.enabled == "true" && length(var.zone_id) > 0 ? "true" : "false"
+  namespace = var.namespace
   name      = local.cluster_dns_name
-#   stage     = var.stage
+  stage     = var.stage
   zone_id   = var.zone_id
-  records   = coalescelist(aws_docdb_cluster.default.*.endpoint, [""])
+  records   = [coalescelist(aws_docdb_cluster.default.*.endpoint, [""])]
 }
 
 module "dns_replicas" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.3.0"
-#   source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.2.6"
-  enabled   = var.enabled && length(var.zone_id) > 0 ? "true" : "false"
-#   namespace = var.namespace
+  source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.2.6"
+  enabled   = var.enabled == "true" && length(var.zone_id) > 0 ? "true" : "false"
+  namespace = var.namespace
   name      = local.reader_dns_name
-#   stage     = var.stage
+  stage     = var.stage
   zone_id   = var.zone_id
-  records   = coalescelist(aws_docdb_cluster.default.*.reader_endpoint, [""])
+  records   = [coalescelist(aws_docdb_cluster.default.*.reader_endpoint, [""])]
 }
 
